66346206ec995cad05b828229d3c6f74b196a527
Rémy Peru <peru.remy@gmail.com>
2015-01-25 22:33:05 +0100

    fixed gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
 
-gulp.task('browser-sync', function() {
-  browserSync({
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync.init(null, {
     proxy: "localhost:3000"
   });
 });
 
-gulp.task('default', ['browser-sync'], function() {
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch(["public/js/**/*.js", "./*.html"], reload);
+});
+
+gulp.task('nodemon', function (cb) {
+  var called = false;
+  return nodemon({script: './bin/www'}).on('start', function () {
+    if (!called) {
+      called = true;
+      cb();
+    }
+  });
 });
