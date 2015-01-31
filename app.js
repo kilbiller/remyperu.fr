f5f1adc4965622e494eb02fcc994505525f68dbc
Rémy Peru <peru.remy@gmail.com>
2015-01-31 13:41:14 +0100

    added compression

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var bodyParser = require('body-parser');
 var session = require('express-session')
 var lusca = require('lusca');
 var errorhandler = require('errorhandler');
+var compress = require('compression');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.use(compress());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
