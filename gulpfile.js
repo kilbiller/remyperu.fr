c73930c725e31c349acea97b62eb457c21ade4bf
Rémy Peru <peru.remy@gmail.com>
2015-02-10 14:39:46 +0100

    now use autoprefixer to prefix css files & updated browser-sync to 2.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var nodemon = require('gulp-nodemon');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer-core');
 
-gulp.task('browser-sync', ['sass', 'nodemon'], function() {
-  browserSync.init(null, {
+gulp.task('browser-sync', ['css', 'nodemon'], function() {
+  browserSync.init({
     proxy: "localhost:3000"
   });
 });
@@ -20,14 +22,15 @@ gulp.task('nodemon', function (cb) {
   });
 });
 
-gulp.task('sass', function () {
-gulp.src('scss/app.scss')
+gulp.task('css', function () {
+  gulp.src('scss/app.scss')
   .pipe(sass())
+  .pipe(postcss([ autoprefixer({ browsers: ['last 2 version'] }) ]))
   .pipe(gulp.dest('public/stylesheets'))
   .pipe(reload({stream:true}));
 });
 
 gulp.task('default', ['browser-sync'], function () {
-  gulp.watch("scss/*.scss", ['sass']);
+  gulp.watch("scss/*.scss", ['css']);
   gulp.watch(["views/**/**", "controllers/*"], reload);
 });
