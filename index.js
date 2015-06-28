715e73bc0a1596767f589fc15156806b20d5db4c
Rémy Peru <peru.remy@gmail.com>
2015-06-28 17:10:45 +0200

    removed unused dependencies

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var path = require("path");
 var favicon = require("serve-favicon");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
-var session = require("express-session");
 var errorhandler = require("errorhandler");
 var compress = require("compression");
 
