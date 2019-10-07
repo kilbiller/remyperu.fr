b2793a27e92f6dcab70f87fe454d7fdbaf6b146c
Rémy Peru <peru.remy@gmail.com>
2019-10-07 22:27:21 +0200

    Use html5 semantic elements

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Main: React.FunctionComponent = ({ children }) => {
-  return <main className="ph6-l ph4 f4">{children}</main>;
+  return (
+    <main className="ph6-l ph4 f4" role="main">
+      {children}
+    </main>
+  );
 };
 
 export default Main;
