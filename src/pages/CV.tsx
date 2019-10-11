d88b8ed7a130b4612a17827c4076d668d597f9fb
Rémy Peru <peru.remy@gmail.com>
2019-10-11 13:22:41 +0200

    Use Link component

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -31,23 +31,19 @@ const CV: React.SFC = () => {
         </h2>
         <p>
           <span className="b">August 2015 - present:</span> Web developer{" "}
-          <a href="https://www.invityou.com" className="link blue dim">
-            @Corpo'Events
-          </a>
+          <Link href="https://www.invityou.com">@Corpo'Events</Link>
         </p>
         <p>
           <span className="b">November 2013 - April 2014:</span> Hybrid app
           developer (Intern){" "}
-          <a href="https://www.cirad.fr/en/home-page" className="link blue dim">
+          <Link href="https://www.cirad.fr/en/home-page">
             @CIRAD Montpellier
-          </a>
+          </Link>
         </p>
         <p>
           <span className="b">April 2012 - July 2012:</span> PHP developer
           (Intern){" "}
-          <a href="https://www.ville-arles.fr/mairie" className="link blue dim">
-            @Mairie d'Arles
-          </a>
+          <Link href="https://www.ville-arles.fr/mairie">@Mairie d'Arles</Link>
         </p>
         <h2 className="f2">
           <span className="bg-yellow ph2">Education</span>
@@ -55,28 +51,20 @@ const CV: React.SFC = () => {
         <p>
           <span className="b">2012 - 2013:</span> Bachelor's degree (Licence) in
           computer science{" "}
-          <a href="https://www.umontpellier.fr/en/" className="link blue dim">
+          <Link href="https://www.umontpellier.fr/en/">
             @Montpellier 2 University
-          </a>
+          </Link>
         </p>
         <p>
           <span className="b">2010 - 2012:</span> University Degree (DUT) in
           computer science{" "}
-          <a
-            href="https://iut.univ-amu.fr/sites/arles"
-            className="link blue dim"
-          >
-            @IUT d'Arles
-          </a>
+          <Link href="https://iut.univ-amu.fr/sites/arles">@IUT d'Arles</Link>
         </p>
         <p className="mt4 mt5-ns">
           You can get my full resume in PDF{" "}
-          <a
-            href="https://drive.google.com/uc?export=download&id=0B-HUrU55uuTtTHlSNWdqVzZMaVk"
-            className="link blue dim"
-          >
+          <Link href="https://drive.google.com/uc?export=download&id=0B-HUrU55uuTtTHlSNWdqVzZMaVk">
             here
-          </a>
+          </Link>
           .
         </p>
       </Main>
