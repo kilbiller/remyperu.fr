da415c768237c66ed9a8bb804839f5bc6507ec6b
Rémy Peru <peru.remy@gmail.com>
2019-10-12 09:17:48 +0200

    Add purgecss

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['src/**/*.tsx'],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+module.exports = {
+  plugins: [require('tailwindcss'), require('autoprefixer'), purgecss]
+};
