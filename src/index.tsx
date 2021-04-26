9462e3e9567ddbc5d44a89162dc433709fb63123
Rémy <peru.remy@gmail.com>
2021-04-26 17:39:39 +0200

    Update deployment

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import Router from 'koa-router';
 import serve from 'koa-static';
 import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
+import { networkInterfaces } from 'os';
 
 import Home from './pages/Home';
 import CV from './pages/CV';
@@ -35,6 +36,22 @@ router.get('/contact', (ctx, next) => {
 
   ctx.type = 'html';
   ctx.body = html;
+});
+
+router.get('/kubernetes', (ctx, next) => {
+  const interfaces = networkInterfaces();
+
+  const [{ address }] = Object.values(
+    (interfaces as unknown) as any[]
+  ).flatMap((interfaces: any[]) =>
+    interfaces.filter((prop) => prop.family === 'IPv4' && !prop.internal)
+  );
+
+  ctx.type = 'json';
+  ctx.body = {
+    host_ip: address,
+    your_ip: ctx.req.headers['x-forwarded-for'] || ctx.req.socket.remoteAddress,
+  };
 });
 
 app.use(router.routes()).use(router.allowedMethods());
